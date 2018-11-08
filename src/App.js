1363190739625b727c8174109dec3f6523622a3e
김성훈 <seongh7800@douzone.com>
2018-11-08 16:49:52 +0900

    디렉토리 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+
+        {/* Nav bar */}
+        <nav class="navbar navbar-light bg-light">
+          <a class="navbar-brand">
+            <img src="http://getbootstrap.com/docs/4.1/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" />
+            &nbsp;&nbsp;Contact
           </a>
-        </header>
+          <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+            {/* <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
+          </form>
+        </nav>
+        {/* Nav bar */}
+
+
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Morbi leo risus</li>
+          <li class="list-group-item">Porta ac consectetur ac</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+
       </div>
     );
   }
