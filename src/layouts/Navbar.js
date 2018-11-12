7b34f0bd39fa00fe9044872986d298595f4877cd
김성훈 <seongh7800@douzone.com>
2018-11-12 14:56:38 +0900

    테스트 코드 추가

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -18,6 +18,11 @@ class Navbar extends Component {
                 <a href="/#/" className="navbar-brand">
                     &nbsp;{appName}
                 </a>
+                {/* 
+                <form className="form-inline">
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="button">추가</button>
+                </form> 
+                */}
             </nav>
         )// return 
     } // render 
